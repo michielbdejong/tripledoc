093c789ddf3e7af20200946c75ff013717ba75c9
Vincent <git@vincenttunru.com>
2019-08-29 11:45:25 +0200

    Update repository URL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://vinnl.gitlab.io', // Your website URL
+  url: 'https://vincenttunru.gitlab.io', // Your website URL
   baseUrl: '/tripledoc/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'tripledoc',
-  organizationName: 'vinnl',
+  organizationName: 'vincenttunru',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
